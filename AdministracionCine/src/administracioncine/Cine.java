280f31b87dc44ba28d55ae3cf9bbf8b4b3a2db5b
DouglasRivera <riveradouglas19@gmail.com>
2020-11-30 12:02:59 -0600

    Aqui esta

---
+++ AdministracionCine/src/administracioncine/Cine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package administracioncine;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author famii
+ */
+public class Cine {
+    String Mision;
+    String Vision;
+    String Logo;
+    String Ciudad;
+    String Ubicacion;
+    ArrayList <Sala> Salas = new ArrayList();
+}
